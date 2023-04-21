b8a5300d7347de25d55d27716ca4f032942187ff
Krakv <abliten@mail.ru>
2023-04-21 20:34:53 +0500

    Создание ленты и перемещение по ней

--- AlgorithmTuringInterface/Form1.Designer.cs
+++ AlgorithmTuringInterface/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace AlgorithmTuringInterface
+﻿using System.Windows.Forms;
+
+namespace AlgorithmTuringInterface
 {
     partial class Form1
     {
@@ -28,13 +30,244 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.Tape = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.textBox19 = new System.Windows.Forms.TextBox();
+            this.textBox20 = new System.Windows.Forms.TextBox();
+            this.textBox17 = new System.Windows.Forms.TextBox();
+            this.textBox18 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.NextElement = new System.Windows.Forms.Button();
+            this.PreviousElement = new System.Windows.Forms.Button();
+            this.Tape.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Tape
+            // 
+            resources.ApplyResources(this.Tape, "Tape");
+            this.Tape.Controls.Add(this.textBox11, 0, 1);
+            this.Tape.Controls.Add(this.textBox14, 0, 1);
+            this.Tape.Controls.Add(this.textBox15, 0, 1);
+            this.Tape.Controls.Add(this.textBox12, 0, 1);
+            this.Tape.Controls.Add(this.textBox13, 0, 1);
+            this.Tape.Controls.Add(this.textBox16, 0, 1);
+            this.Tape.Controls.Add(this.textBox19, 0, 1);
+            this.Tape.Controls.Add(this.textBox20, 0, 1);
+            this.Tape.Controls.Add(this.textBox17, 0, 1);
+            this.Tape.Controls.Add(this.textBox18, 0, 1);
+            this.Tape.Controls.Add(this.textBox4, 0, 0);
+            this.Tape.Controls.Add(this.textBox5, 0, 0);
+            this.Tape.Controls.Add(this.textBox3, 0, 0);
+            this.Tape.Controls.Add(this.textBox1, 0, 0);
+            this.Tape.Controls.Add(this.textBox2, 0, 0);
+            this.Tape.Controls.Add(this.textBox6, 0, 0);
+            this.Tape.Controls.Add(this.textBox9, 0, 0);
+            this.Tape.Controls.Add(this.textBox10, 0, 0);
+            this.Tape.Controls.Add(this.textBox7, 0, 0);
+            this.Tape.Controls.Add(this.textBox8, 0, 0);
+            this.Tape.Name = "Tape";
+            this.Tape.Paint += new System.Windows.Forms.PaintEventHandler(this.Tape_Paint);
+            // 
+            // textBox11
+            // 
+            resources.ApplyResources(this.textBox11, "textBox11");
+            this.textBox11.Name = "textBox11";
+            this.textBox11.ReadOnly = true;
+            // 
+            // textBox14
+            // 
+            resources.ApplyResources(this.textBox14, "textBox14");
+            this.textBox14.Name = "textBox14";
+            this.textBox14.ReadOnly = true;
+            // 
+            // textBox15
+            // 
+            resources.ApplyResources(this.textBox15, "textBox15");
+            this.textBox15.Name = "textBox15";
+            this.textBox15.ReadOnly = true;
+            // 
+            // textBox12
+            // 
+            resources.ApplyResources(this.textBox12, "textBox12");
+            this.textBox12.Name = "textBox12";
+            this.textBox12.ReadOnly = true;
+            // 
+            // textBox13
+            // 
+            resources.ApplyResources(this.textBox13, "textBox13");
+            this.textBox13.Name = "textBox13";
+            this.textBox13.ReadOnly = true;
+            // 
+            // textBox16
+            // 
+            resources.ApplyResources(this.textBox16, "textBox16");
+            this.textBox16.Name = "textBox16";
+            this.textBox16.ReadOnly = true;
+            // 
+            // textBox19
+            // 
+            resources.ApplyResources(this.textBox19, "textBox19");
+            this.textBox19.Name = "textBox19";
+            this.textBox19.ReadOnly = true;
+            // 
+            // textBox20
+            // 
+            resources.ApplyResources(this.textBox20, "textBox20");
+            this.textBox20.Name = "textBox20";
+            this.textBox20.ReadOnly = true;
+            // 
+            // textBox17
+            // 
+            resources.ApplyResources(this.textBox17, "textBox17");
+            this.textBox17.Name = "textBox17";
+            this.textBox17.ReadOnly = true;
+            // 
+            // textBox18
+            // 
+            resources.ApplyResources(this.textBox18, "textBox18");
+            this.textBox18.Name = "textBox18";
+            this.textBox18.ReadOnly = true;
+            // 
+            // textBox4
+            // 
+            resources.ApplyResources(this.textBox4, "textBox4");
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            // 
+            // textBox5
+            // 
+            resources.ApplyResources(this.textBox5, "textBox5");
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            // 
+            // textBox6
+            // 
+            resources.ApplyResources(this.textBox6, "textBox6");
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            // 
+            // textBox9
+            // 
+            resources.ApplyResources(this.textBox9, "textBox9");
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            // 
+            // textBox10
+            // 
+            resources.ApplyResources(this.textBox10, "textBox10");
+            this.textBox10.Name = "textBox10";
+            this.textBox10.ReadOnly = true;
+            // 
+            // textBox7
+            // 
+            resources.ApplyResources(this.textBox7, "textBox7");
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            // 
+            // textBox8
+            // 
+            resources.ApplyResources(this.textBox8, "textBox8");
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            // 
+            // NextElement
+            // 
+            resources.ApplyResources(this.NextElement, "NextElement");
+            this.NextElement.Name = "NextElement";
+            this.NextElement.UseVisualStyleBackColor = true;
+            this.NextElement.Click += new System.EventHandler(this.NextElement_Click);
+            // 
+            // PreviousElement
+            // 
+            resources.ApplyResources(this.PreviousElement, "PreviousElement");
+            this.PreviousElement.Name = "PreviousElement";
+            this.PreviousElement.UseVisualStyleBackColor = true;
+            this.PreviousElement.Click += new System.EventHandler(this.PreviousElement_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.Controls.Add(this.PreviousElement);
+            this.Controls.Add(this.NextElement);
+            this.Controls.Add(this.Tape);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Tape.ResumeLayout(false);
+            this.Tape.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private TableLayoutPanel Tape;
+        private TextBox textBox8;
+        private TextBox textBox18;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox6;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private TextBox textBox7;
+        private TextBox textBox17;
+        private TextBox textBox11;
+        private TextBox textBox14;
+        private TextBox textBox15;
+        private TextBox textBox12;
+        private TextBox textBox13;
+        private TextBox textBox16;
+        private TextBox textBox19;
+        private TextBox textBox20;
+        private Button NextElement;
+        private Button PreviousElement;
     }
 }
 
--- AlgorithmTuringInterface/Form1.cs
+++ AlgorithmTuringInterface/Form1.cs
@@ -12,9 +12,63 @@ namespace AlgorithmTuringInterface
 {
     public partial class Form1 : Form
     {
+        Dictionary<int, string> tape = new Dictionary<int, string>();
+        int shift = 0;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InitializeTape();
+        }
+
+        private void PreviousElement_Click(object sender, EventArgs e)
+        {
+            shift--;
+            InitializeTape();
+        }
+        private void NextElement_Click(object sender, EventArgs e)
+        {
+            shift++;
+            InitializeTape();
+        }
+
+        private void Tape_Paint(object sender, PaintEventArgs e)
+        {
+            tape[-4] = "0";
+            tape[-1] = "1";
+            tape[0] = "0";
+            tape[1] = "1";
+            InitializeTape();
+        }
+
+        private void InitializeTape()
+        {
+            this.textBox1.Text = (-4 + shift).ToString();
+            this.textBox2.Text = (-3 + shift).ToString();
+            this.textBox3.Text = (-2 + shift).ToString();
+            this.textBox4.Text = (-1 + shift).ToString();
+            this.textBox5.Text = (0 + shift).ToString();
+            this.textBox6.Text = (1 + shift).ToString();
+            this.textBox7.Text = (2 + shift).ToString();
+            this.textBox8.Text = (3 + shift).ToString();
+            this.textBox9.Text = (4 + shift).ToString();
+            this.textBox10.Text = (5 + shift).ToString();
+            try { this.textBox11.Text = tape[-4 + shift]; } catch { this.textBox11.Text = ""; }
+            try { this.textBox12.Text = tape[-3 + shift]; } catch { this.textBox12.Text = ""; }
+            try { this.textBox13.Text = tape[-2 + shift]; } catch { this.textBox13.Text = ""; }
+            try { this.textBox14.Text = tape[-1 + shift]; } catch { this.textBox14.Text = ""; }
+            try { this.textBox15.Text = tape[0 + shift]; } catch { this.textBox15.Text = ""; }
+            try { this.textBox16.Text = tape[1 + shift]; } catch { this.textBox16.Text = ""; }
+            try { this.textBox17.Text = tape[2 + shift]; } catch { this.textBox17.Text = ""; }
+            try { this.textBox18.Text = tape[3 + shift]; } catch { this.textBox18.Text = ""; }
+            try { this.textBox19.Text = tape[4 + shift]; } catch { this.textBox19.Text = ""; }
+            try { this.textBox20.Text = tape[5 + shift]; } catch { this.textBox20.Text = ""; }
+        }
+
+        
     }
 }
