cd1643c28f973ad1dc1fc3eb7a4f66e075477df7
Krakv <abliten@mail.ru>
2023-05-22 15:15:28 +0500

    Скролл таблицы состояний

--- AlgorithmTuringInterface/EditQuantities.Designer.cs
+++ AlgorithmTuringInterface/EditQuantities.Designer.cs
@@ -140,6 +140,7 @@ namespace AlgorithmTuringInterface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(1121, 608);
             this.Controls.Add(this.DelColumnBtn);
             this.Controls.Add(this.ColumnNameTxtBx);
